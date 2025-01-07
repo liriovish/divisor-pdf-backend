079b6f29113eb192aba449c05a09aa2c4692b787
desenvolvedor39.webcartorios <desenvolvedor39.webcartorios@gmail.com>
2025-01-07 10:31:35 -0300

    Ajustes usecase

--- src/domain/usecases/api-usecase.js
+++ src/domain/usecases/api-usecase.js
@@ -75,7 +75,7 @@ module.exports = class ApiUseCase {
                         console.log(`Arquivo removido de uploads: ${sCaminhoUpload}`);
                     }
                 });
-            }, 5 * 60 * 1000); 
+            }, 30 * 60 * 1000); 
     
             /**
              * Retorna a resposta de sucesso com os detalhes do PDF
@@ -99,19 +99,11 @@ module.exports = class ApiUseCase {
      */
     async divisaoPersonalizada(oDados) {
         try {
-            /**
-             * Desestrutura os dados recebidos e obtém o nome original do arquivo paginaInicial e paginaFinal
-             * @var {string} originalname 
-             * @var {number} paginaInicial 
-             * @var {number} paginaFinal 
-             */
-            const { originalname, paginaInicial, paginaFinal } = oDados;
-    
              /**
              * Define o caminho de upload
              * @var {string} sCaminhoUpload 
              */
-            const sCaminhoUpload = path.join(__dirname, '../../../uploads', `${originalname}.pdf`);
+            const sCaminhoUpload = path.join(__dirname, '../../../uploads', `${oDados.originalname}.pdf`);
     
             
             if (!fs.existsSync(sCaminhoUpload)) {
@@ -137,14 +129,14 @@ module.exports = class ApiUseCase {
             const iTotalPaginas = oDocumento.getPageCount();
     
             // Verifica se os parâmetros de página são válidos
-            if (isNaN(paginaInicial) || isNaN(paginaFinal)) {
+            if (isNaN(oDados.paginaInicial) || isNaN(oDados.paginaFinal)) {
                 return HttpResponse.badRequest({ message: 'Os valores de página devem ser números válidos.' });
             }
 
             // Gera um array de índices representando as páginas a serem copiadas
             const aIndices = Array.from(
-                { length: paginaFinal - paginaInicial + 1 },
-                (_, i) => (paginaInicial - 1) + i
+                { length: oDados.paginaFinal - oDados.paginaInicial + 1 },
+                (_, i) => (oDados.paginaInicial - 1) + i
             );
     
             // Verifica se algum índice está fora do intervalo válido de páginas
@@ -174,7 +166,7 @@ module.exports = class ApiUseCase {
              * Copia as páginas do documento original para o novo documento
              * @var {string} aPaginas
              */
-            const sNovoArquivoNome = `${originalname}-${paginaInicial}-${paginaFinal}.pdf`;
+            const sNovoArquivoNome = `${oDados.originalname}-${oDados.paginaInicial}-${oDados.paginaFinal}.pdf`;
     
             /**
              *  Define o caminho completo para salvar o novo arquivo PDF dividido
@@ -205,165 +197,128 @@ module.exports = class ApiUseCase {
     async divisaoFixa(oDados) {
         try {
             /**
-             * Desestrutura os dados recebidos e obtém o nome original do arquivo e o intervalo
-             * @var {string} originalname
-             * @var {number} intervalo
+             * Define o caminho do arquivo original na pasta de uploads
+             * @var {string} sCaminhoUpload 
              */
-            const { originalname, intervalo } = oDados;
+            const sCaminhoUpload = path.join(__dirname, '../../../uploads', `${oDados.originalname}.pdf`);
     
-            /**
-             * Define o caminho do arquivo original a ser processado
-             * @var {string} sCaminhoUpload
-             */
-            const sCaminhoUpload = path.join(__dirname, '../../../uploads', `${originalname}.pdf`);
-    
-            /**
-             * Verifica se o arquivo existe no sistema de arquivos
-             */
+            // Verifica se o arquivo original existe no sistema de arquivos
             if (!fs.existsSync(sCaminhoUpload)) {
                 return HttpResponse.badRequest({ message: 'Arquivo não encontrado.' });
             }
     
             /**
-             * Lê o arquivo PDF a partir do sistema de arquivos
-             * @var {Buffer} pdfBytes
+             * Lê o conteúdo binário do arquivo PDF
+             * @var {Buffer} pdfBytes 
              */
             const pdfBytes = fs.readFileSync(sCaminhoUpload);
     
             /**
-             * Verifica se o conteúdo do arquivo é válido
-             */
-            if (!pdfBytes || pdfBytes.length === 0) {
-                return HttpResponse.badRequest({ message: 'Arquivo PDF inválido ou vazio.' });
-            }
-    
-            /**
              * Carrega o documento PDF utilizando a biblioteca pdf-lib
-             * @var {PDFDocument} oDocumento
+             * @var {PDFDocument} oDocumento 
              */
             const oDocumento = await PDFDocument.load(pdfBytes);
-            
+    
             /**
-             * Obtém o total de páginas no arquivo PDF
-             * @var {number} iTotalPaginas
+             * Obtém o número total de páginas do PDF
+             * @var {number} iTotalPaginas 
              */
             const iTotalPaginas = oDocumento.getPageCount();
     
-            /**
-             * Verifica se o intervalo é válido
-             * Se o intervalo não for válido, retorna erro 400
-             */
-            if (isNaN(intervalo) || intervalo < 1 || intervalo > iTotalPaginas) {
+            // Valida se o intervalo de páginas é válido
+            if (isNaN(oDados.intervalo) || oDados.intervalo < 1 || oDados.intervalo > iTotalPaginas) {
                 return HttpResponse.badRequest({ message: 'Intervalo de páginas inválido.' });
             }
     
             /**
-             * Calcula o número de partes que o PDF será dividido
-             * @var {number} numDivisoes
+             * Calcula a quantidade de divisões necessárias com base no intervalo informado
+             * @var {number} iDivisoes 
              */
-            const numDivisoes = Math.ceil(iTotalPaginas / intervalo);
-            
+            const iDivisoes = Math.ceil(iTotalPaginas / oDados.intervalo);
+    
             /**
-             * Cria uma lista de nomes dos arquivos PDF gerados
-             * @var {Array<string>} aNomesArquivos
+             * Array para armazenar os nomes dos arquivos PDF gerados
+             * @var {Array<string>} aNomesArquivos 
              */
             const aNomesArquivos = [];
     
-            /**
-             * Divide o PDF em partes com o intervalo fixo e cria os arquivos PDF
-             */
-            for (let i = 0; i < numDivisoes; i++) {
+            // Loop para dividir o PDF em partes com base no intervalo
+            for (let i = 0; i < iDivisoes; i++) {
+                /**
+                 * Cria um novo documento PDF para cada divisão
+                 * @var {PDFDocument} oNovoPdf 
+                 */
                 const oNovoPdf = await PDFDocument.create();
-                const inicio = i * intervalo;
-                const fim = Math.min((i + 1) * intervalo, iTotalPaginas);
     
                 /**
-                 * Cria os índices das páginas que serão copiadas para este novo arquivo
-                 * @var {Array<number>} aIndices - Índices das páginas a serem copiadas
+                 * Determina o índice inicial e final das páginas para a divisão atual
+                 * @var {number} iInicio
+                 * @var {number} iFim
                  */
-                const aIndices = Array.from({ length: fim - inicio }, (_, j) => inicio + j);
+                const iInicio = i * oDados.intervalo;
+                const iFim = Math.min((i + 1) * oDados.intervalo, iTotalPaginas);
     
                 /**
-                 * Copia as páginas do documento original para o novo documento
+                 * Gera os índices das páginas a serem copiadas para o novo documento
+                 * @var {Array<number>} aIndices 
                  */
+                const aIndices = Array.from({ length: iFim - iInicio }, (_, j) => iInicio + j);
+    
+                // Copia as páginas especificadas do documento original para o novo documento
                 const aPaginas = await oNovoPdf.copyPages(oDocumento, aIndices);
                 aPaginas.forEach((pagina) => oNovoPdf.addPage(pagina));
     
                 /**
-                 * Salva o novo arquivo PDF
-                 * @var {Buffer} novoPdfBytes - Conteúdo binário do novo PDF
+                 * Salva o novo documento PDF em formato binário
+                 * @var {Buffer} novoPdfBytes 
                  */
                 const novoPdfBytes = await oNovoPdf.save();
-                const novoPdfNome = `${originalname}-${i + 1}.pdf`;
-                const sCaminhoNovoPdf = path.join(__dirname, '../../../downloads', novoPdfNome);
     
                 /**
-                 * Salva o novo arquivo no sistema de arquivos
+                 * Define o nome e o caminho do novo arquivo PDF gerado
+                 * @var {string} sNovoPdfNome 
+                 * @var {string} sCaminhoNovoPdf 
                  */
+                const sNovoPdfNome = `${oDados.originalname}-${i + 1}.pdf`;
+                const sCaminhoNovoPdf = path.join(__dirname, '../../../downloads', sNovoPdfNome);
+    
+                // Salva o novo arquivo PDF no sistema de arquivos
                 fs.writeFileSync(sCaminhoNovoPdf, novoPdfBytes);
     
-                /**
-                 * Adiciona o nome do arquivo gerado à lista
-                 */
-                aNomesArquivos.push(novoPdfNome);
+                // Adiciona o nome do arquivo gerado à lista
+                aNomesArquivos.push(sNovoPdfNome);
             }
     
             /**
-            * Define o arquivo final
-            * @var {string} sArquivoFinal
-            */
-            let sArquivoFinal;
-           
-            /**
-            * Caso o número de arquivos gerados seja maior que 2, cria um arquivo ZIP
-            */
-            if (aNomesArquivos.length > 2) {
-                /**
-                 * Cria um arquivo ZIP contendo todos os PDFs gerados
-                 * @var {string} sZipNome 
-                 * @var {string} sCaminhoZip 
-                 */
-                const sZipNome = `${originalname}-dividido.zip`;
-                const sCaminhoZip = path.join(__dirname, '../../../downloads', sZipNome);
-    
-                /**
-                 * Cria um arquivo temporário para armazenar o ZIP
-                 * @var {object} oZip
-                 */
-                const oZip = new AdmZip();
-    
-                aNomesArquivos.forEach((nomeArquivo) => {
-                    const pdfPath = path.join(__dirname, '../../../downloads', nomeArquivo);
-                    oZip.addLocalFile(pdfPath);
-                });
-    
-                /**
-                 * Salva o arquivo ZIP no sistema de arquivos
-                 */
-                oZip.writeZip(sCaminhoZip);
-    
-                /**
-                 * Deleta os PDFs gerados após criar o ZIP
-                 */
-                aNomesArquivos.forEach((nomeArquivo) => {
-                    const pdfPath = path.join(__dirname, '../../../downloads', nomeArquivo);
-                    fs.unlinkSync(pdfPath);  
-                });
-    
-                /**
-                 * Define o nome do arquivo ZIP como resposta final
-                 */
-                sArquivoFinal = sZipNome;
-            } else {
-                /**
-                 * Caso haja 2 ou menos arquivos, retorna os nomes dos arquivos gerados
-                 */
-                sArquivoFinal = aNomesArquivos;
-            }
-    
-            /**
-             * Retorna a resposta de sucesso com o nome do arquivo (ou lista de nomes)
+             * Cria um arquivo ZIP para armazenar os PDFs gerados
+             * @var {string} sZipNome 
+             * @var {string} sCaminhoZip 
              */
+            const sZipNome = `${oDados.originalname}-dividido.zip`;
+            const sCaminhoZip = path.join(__dirname, '../../../downloads', sZipNome);
+            const oZip = new AdmZip();
+    
+            // Adiciona todos os PDFs gerados ao arquivo ZIP
+            aNomesArquivos.forEach((nomeArquivo) => {
+                const pdfPath = path.join(__dirname, '../../../downloads', nomeArquivo);
+                oZip.addLocalFile(pdfPath);
+            });
+    
+            // Salva o arquivo ZIP no sistema de arquivos
+            oZip.writeZip(sCaminhoZip);
+    
+            // Remove os arquivos PDF individuais após criar o ZIP
+            aNomesArquivos.forEach((nomeArquivo) => {
+                const pdfPath = path.join(__dirname, '../../../downloads', nomeArquivo);
+                fs.unlinkSync(pdfPath);
+            });
+    
+            /**
+             * Retorna o nome do arquivo ZIP gerado como resposta
+             * @var {string} sArquivoFinal 
+             */
+            const sArquivoFinal = sZipNome;
+    
             return HttpResponse.ok({
                 message: 'PDF dividido com sucesso.',
                 arquivo: sArquivoFinal,
@@ -376,19 +331,18 @@ module.exports = class ApiUseCase {
             });
         }
     }
+    
 
     /**
      * Função responsável por baixar o arquivo PDF
      */
     async baixarArquivo(oDados) {
         try {
-            const { originalname } = oDados;
-
             /**
              * Define o caminho completo do arquivo a ser baixado
              * @var {string} sCaminhoDownload
              */
-            const sCaminhoDownload = path.join(__dirname, '../../../downloads', originalname);
+            const sCaminhoDownload = path.join(__dirname, '../../../downloads', oDados.originalname);
 
             /**
              * Verifica se o arquivo existe no sistema de arquivos
